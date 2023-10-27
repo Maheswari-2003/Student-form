4633f8d03e075a1aba9a821155cceef9de256990
Maheswari-2003 <89957091+Maheswari-2003@users.noreply.github.com>
2023-10-27 12:45:24 +0530

    Add files via upload

---
+++ Form frontend/axios.js
@@ -0,0 +1,41 @@
+/*const axios = require('axios');
+const create = 'admit';
+const apiUrl = 'http://localhost:8080/admit';
+
+
+const dataToStore = {
+    
+    name:"maha", 
+   dob: "2003-06-21", 
+   mobile: 89765432134, 
+   email:"mahes@gmail.com",
+   address:"34,raja nagar",
+   gender:"female",
+   fatherName:"hari",
+   course:"cse",
+   percentage12th:56
+  };
+
+axios.post(apiUrl, dataToStore, {
+  headers: {
+    'Authorization': `Bearer ${create}`
+  }
+})
+.then(function (response) {
+  // Handle the success response, if needed
+})
+.catch(function (error) {
+  // Handle errors, if any
+});
+
+
+axios.get(apiUrl)
+  .then(function (response) {
+    // Handle the successful response here
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // Handle errors here
+    console.error(error);
+  });
+*/ 
